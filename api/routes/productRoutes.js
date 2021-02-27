77ce13a9cf81a6c283cd97d75a6e4e84591849ae
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-27 23:32:17 +0530

    Created different types of Routes

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -7,4 +7,42 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    res.status(200).json({
+        message: 'Products Data in POST Req',
+        data: req.body
+    });
+});
+
+router.get('/:productId', (req, res) => {
+    const id = req.params.productId;
+    if (id == 'special') {
+        res.status(200).json({
+            message: 'Products Special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'Products Others ID',
+            id: id
+        });
+    }
+});
+
+
+router.patch('/:productId', (req, res) => {
+    res.status(200).json({
+        message: 'Updated Product Data'
+    })
+});
+
+router.delete('/:productId', (req, res) => {
+    const { productId } = req.params;
+    res.status(200).json({
+        message: `${productId} Product is Deleted`
+    })
+});
+
+
+
 module.exports = router;
