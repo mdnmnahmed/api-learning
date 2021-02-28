6806a52ac466c6f6da9b97c1ffbecb22388edf1c
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-28 15:10:01 +0530

    handeling cors

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-// const express = require('express');
 const app = require('express')();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 //Middleares
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); //  https://nnn.web.app
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authoriztion');
 
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+
+//Connection with DB
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then((res) => {
+        console.log("Connected with MongoDB ");
+    })
+    .catch((err) => {
+        console.log("Error in with MongoDB ", err);
+    });
 
 
 //Routes
