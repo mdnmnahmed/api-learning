e87b2017ff84aef7e97b890b3c7bbc4b86267d0a
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-28 10:22:11 +0530

    error handeling

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 // const express = require('express');
 const app = require('express')();
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 //Middleares
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+
+
+//Routes
 app.use('/products', require('./api/routes/productRoutes'));
 app.use('/orders', require('./api/routes/orderRoutes'));
 
-//Routes
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Hello Num'
     });
 });
 
+
+//Handeling Error Routes
+app.use((req, res, next) => {
+    const error = new Error('Route not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 
 //Create Server
